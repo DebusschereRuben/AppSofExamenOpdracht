5e7b81398ff9f31d8b77ef04854d797232e1206b
DaelenVDK <157583854+DaelenVDK@users.noreply.github.com>
2024-03-12 17:32:02 +0100

    aanmaak meal klasse en progress random meal

--- AppSofExamenOpdracht/Pages/RandomPage.xaml.cs
+++ AppSofExamenOpdracht/Pages/RandomPage.xaml.cs
@@ -11,6 +11,7 @@ namespace AppSofExamenOpdracht.Pages
     public partial class RandomPage : Page
     {
         Cocktail currentCocktail;
+        Meal currentMeal;
         private Frame _mainFrame;
         public RandomPage(Frame mainFrame)
         {
@@ -24,7 +25,10 @@ namespace AppSofExamenOpdracht.Pages
 
             var cocktail = await ProcessCocktailAsync(client);
             currentCocktail = cocktail;
+            var meal = await ProcessMealAsync(client);
+            currentMeal = meal;
 
+            LoadMealPreview(meal);
             loadCocktailPreview(cocktail);
         }
 
@@ -41,6 +45,18 @@ namespace AppSofExamenOpdracht.Pages
             cocktail.ingredients = getIngredients(dCocktail);
 
             return cocktail;
+        }
+
+        private async Task<Meal> ProcessMealAsync(HttpClient client)
+        {
+            var JSON = await client.GetStringAsync("https://www.themealdb.com/api/json/v1/1/random.php");
+            var meals = JsonSerializer.Deserialize<Meals>(JSON);
+            var meal = meals.Meals[0];
+
+            // Ingredients list
+            meal.Ingredients = GetIngredients(meal);
+
+            return meal;
         }
         private List<string> getIngredients(Dictionary<string, object> d)
         {
@@ -65,18 +81,54 @@ namespace AppSofExamenOpdracht.Pages
             }
             return ingredients;
         }
+        private List<string> GetIngredients(Meal meal)
+        {
+            List<string> ingredients = new List<string>();
+
+            for (int i = 1; i <= 20; i++)
+            {
+                string ingredientKey = "strIngredient" + i;
+                string measureKey = "strMeasure" + i;
+
+                string? ingredient = Convert.ToString(meal.ContainsKey(ingredientKey) ? meal[ingredientKey] : null);
+                string? measure = Convert.ToString(meal.ContainsKey(measureKey) ? meal[measureKey] : null);
+
+                if (ingredient != null && measure != null && ingredient != "")
+                {
+                    ingredients.Add($"{measure} {ingredient}");
+                }
+                else
+                {
+                    break; // Early exit
+                }
+            }
+            return ingredients;
+        }
 
         private async void loadCocktailPreview(Cocktail cocktail)
         {
             Uri cocktailUri = new Uri(cocktail.Image);
             img_cocktail.Source = new BitmapImage(cocktailUri);
 
-            lbl_name.Content = cocktail.Name;
+            lbl_nameCocktail.Content = cocktail.Name;
+        }
+
+        private async void LoadMealPreview(Meal meal)
+        {
+            Uri mealUri = new Uri(meal.Image);
+            img_meal.Source = new BitmapImage(mealUri);
+
+            lbl_nameMeal.Content = meal.Name;
         }
 
         private async void Go_to_CocktailDetails(object sender, RoutedEventArgs e)
         {
             _mainFrame.Navigate(new CocktailDetailsPage(currentCocktail));
+        }
+
+        private async void Go_to_MealDetails(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new MealDetailsPage(currentMeal));
         }
     }
 }
